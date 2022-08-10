f3a51b8120d138b1739fa509a2432a5e691084e9
xuqunhai <raferxu@163.com>
2022-08-10 21:51:47 +0800

    Vue3 ref语法糖原理剖析

---
+++ 2022/vue3/vite-study/vite-demo/src/components/oldSetup.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1>{{count}}</h1>
+  <button @click="inc">++</button>
+</template>
+
+<script>
+import { ref } from 'vue';
+export default {
+  setup() {
+    const count = ref(0); // 创建响应式数据
+    const inc = () => {
+      count.value++; // 修改响应式数据
+    };
+    return {
+      // return对象用于template使用，即想要在template中使用的东西，必须return出去
+      count,
+      inc
+    };
+  }
+};
+</script>
