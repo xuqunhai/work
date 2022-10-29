7c6769c9bfc9c4d43ef2be0752d4d65b5fd99963
xuqunhai <raferxu@163.com>
2022-10-29 21:55:28 +0800

    useWindowSize判断isMobileTerminal

---
+++ 2022/vue3/vite-study/vue3-middle-stage-raferxu/src/utils/flexible.js
@@ -0,0 +1,6 @@
+import { PC_DEVICE_WIDTH } from '../constants/index.js'
+import { computed } from 'vue'
+import { useWindowSize } from '@vueuse/core'
+
+const { width } = useWindowSize()
+export const isMobileTerminal = computed(() => width.value < PC_DEVICE_WIDTH)
