32f2a689458cc74974d9ba28c787144b64af93e0
xuqunhai <raferxu@163.com>
2022-12-28 16:21:08 +0800

    在 Vuex 中使用 getters 配置来实现 computed 的功能

--- 2022/vue3/geek/geek-admin/src/components/Count.vue
+++ 2022/vue3/geek/geek-admin/src/components/Count.vue
@@ -1,16 +1,16 @@
 <template>
-  <div @click="add">
-    {{ count }}
-  </div>
+  <div @click="add">{{ count }} - {{ double }}</div>
 </template>
 
 <script setup>
 import { computed } from 'vue'
-// import { useStore } from 'vuex'
-import { useStore } from '../store/gvuex'
+import { useStore } from 'vuex'
+// import { useStore } from '../store/gvuex'
 
 let store = useStore()
 let count = computed(() => store.state.count)
+
+let double = computed(() => store.getters.double)
 
 function add() {
   store.commit('add')
--- 2022/vue3/geek/geek-admin/src/store/index.js
+++ 2022/vue3/geek/geek-admin/src/store/index.js
@@ -1,10 +1,15 @@
-// import { createStore } from 'vuex'
-import { createStore } from './gvuex'
+import { createStore } from 'vuex'
+// import { createStore } from './gvuex'
 
 const store = createStore({
   state() {
     return {
       count: 666
+    }
+  },
+  getters: {
+    double(state) {
+      return state.count * 2
     }
   },
   mutations: {
