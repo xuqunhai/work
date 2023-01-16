155a225412ad1941447e67623e04043e7af468d2
xuqunhai <raferxu@163.com>
2023-01-16 22:07:24 +0800

    项目中对实现关注结构的组件的引用

--- 2023/css/cssProject/src/views/mall/shop.vue
+++ 2023/css/cssProject/src/views/mall/shop.vue
@@ -1,14 +1,20 @@
 <template>
-  <c-magazine class="magazine" image="https://cn.vitejs.dev/logo.svg">
-    <div>123</div>
-    <div>123</div>
-    <div>123</div>
+  <c-magazine
+    class="magazine promotion"
+    image="https://cn.vitejs.dev/logo.svg"
+    radius4
+    font-tertiary
+  >
+    <div class="promotion__span" fz14>测试一下</div>
   </c-magazine>
 </template>
 
 <style lang="scss" scoped>
-.magazine {
-  width: 100px;
-  height: 100px;
+@include b(promotion) {
+  @include dimensions(82px);
+  text-align: center;
+  @include e(span) {
+    padding-top: 50px;
+  }
 }
 </style>
