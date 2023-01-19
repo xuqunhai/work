0037346fa7db02f1b63131f1457b77aa362f5953
xuqunhai <raferxu@163.com>
2023-01-19 23:25:45 +0800

    实现promise的resolve

--- 2023/js/promise/promise.js
+++ 2023/js/promise/promise.js
@@ -1,5 +1,10 @@
 function Promise(excutor) {
-  function resolve(data) {}
+  this.promiseStatus = 'pending'
+  this.promsieResult = null
+  function resolve(data) {
+    this.promiseStatus = 'resolved'
+    this.promiseStatus = data
+  }
   function reject(data) {}
   excutor(resolve, reject)
 }
