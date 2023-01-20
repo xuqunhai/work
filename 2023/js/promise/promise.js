1b8726310b8e84766faea91788938305924dbdde
xuqunhai <raferxu@163.com>
2023-01-20 22:45:29 +0800

    then方法完善和优化

--- 2023/js/promise/promise.js
+++ 2023/js/promise/promise.js
@@ -23,70 +23,37 @@ function Promise(excutor) {
 }
 Promise.prototype.then = function (resFn, rejFn) {
   const _this = this
+  const cb = function (type) {
+    try {
+      // 不仅回调，还要处理返回值，则外面包一层函数，里面一并处理
+      let result = type(_this.promsieResult)
+      if (result instanceof Promise) {
+        // 想获取promise对象状态，可通过then拿到
+        result.then(
+          (v) => resolve(v),
+          (r) => reject(r)
+        )
+      } else {
+        resolve(result)
+      }
+    } catch (error) {
+      reject(error)
+    }
+  }
   return new Promise((resolve, reject) => {
     if (this.promiseStatus === 'resolved') {
-      try {
-        let result = resFn(this.promsieResult)
-        if (result instanceof Promise) {
-          result.then(
-            (v) => resolve(v),
-            (r) => reject(r)
-          )
-        } else {
-          resolve(result)
-        }
-      } catch (error) {
-        reject(error)
-      }
+      cb(resFn)
     }
     if (this.promiseStatus === 'rejected') {
-      try {
-        let result = rejFn(this.promsieResult)
-        if (result instanceof Promise) {
-          result.then(
-            (v) => resolve(v),
-            (r) => reject(r)
-          )
-        } else {
-          resolve(result)
-        }
-      } catch (error) {
-        reject(error)
-      }
+      cb(rejFn)
     }
     if (this.promiseStatus === 'pending') {
       this.callback.push({
         resFn: function () {
-          try {
-            // 不仅回调，还要处理返回值，则外面包一层函数，里面一并处理
-            let result = resFn(_this.promsieResult)
-            if (result instanceof Promise) {
-              // 想获取promise对象状态，可通过then拿到
-              result.then(
-                (v) => resolve(v),
-                (r) => reject(r)
-              )
-            } else {
-              resolve(result)
-            }
-          } catch (error) {
-            reject(error)
-          }
+          cb(resFn)
         },
         rejFn: function () {
-          try {
-            let result = rejFn(_this.promsieResult)
-            if (result instanceof Promise) {
-              result.then(
-                (v) => resolve(v),
-                (r) => reject(r)
-              )
-            } else {
-              resolve(result)
-            }
-          } catch (error) {
-            reject(error)
-          }
+          cb(rejFn)
         }
       })
     }
