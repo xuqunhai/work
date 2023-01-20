964af5b114ccb4d02ae38e6f9a97774bcce7c959
xuqunhai <raferxu@163.com>
2023-01-20 21:25:31 +0800

    异步任务回调的执行

--- 2023/js/promise/promise.js
+++ 2023/js/promise/promise.js
@@ -1,16 +1,23 @@
 function Promise(excutor) {
   this.promiseStatus = 'pending'
   this.promsieResult = null
+  this.callback = {}
   var _this = this
   function resolve(data) {
     if (_this.promiseStatus !== 'pending') return
     _this.promiseStatus = 'resolved'
     _this.promsieResult = data
+    if (_this.callback.resFn) {
+      _this.callback.resFn(_this.promsieResult)
+    }
   }
   function reject(data) {
     if (_this.promiseStatus !== 'pending') return
     _this.promiseStatus = 'rejected'
     _this.promsieResult = data
+    if (_this.callback.rejFn) {
+      _this.callback.rejFn(_this.promsieResult)
+    }
   }
   try {
     excutor(resolve, reject)
@@ -24,5 +31,11 @@ Promise.prototype.then = function (resFn, rejFn) {
   }
   if (this.promiseStatus === 'rejected') {
     rejFn(this.promsieResult)
+  }
+  if (this.promiseStatus === 'pending') {
+    this.callback = {
+      resFn,
+      rejFn
+    }
   }
 }
