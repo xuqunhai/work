2525b3c7bc99c0e699cb3d3c82cb43ef89f85934
xuqunhai <raferxu@163.com>
2023-01-21 22:41:54 +0800

    Promise.race方法封装（返回promise取最先改变状态的入参promise）

--- 2023/js/promise/promise.js
+++ 2023/js/promise/promise.js
@@ -107,3 +107,14 @@ Promise.all = function (promises) {
     }
   })
 }
+
+Promise.race = function (promises) {
+  return new Promise((resolve, reject) => {
+    for (let i = 0; i < promises.length; i++) {
+      promises[i].then(
+        (v) => resolve(v),
+        (r) => reject(r)
+      )
+    }
+  })
+}
